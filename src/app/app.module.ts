380ec5df73bf31f188b6db83cdcbf9b6707a13dc
omgitschester <omgitschester@gmail.com>
2017-10-31 11:24:04 -0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyfirstcomponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
